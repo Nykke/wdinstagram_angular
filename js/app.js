e372790a84cca7131a27784a3e8a6436ee1344ab
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-09 08:19:36 -0500

    routes,controllers for index/show

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
 "use strict";
 
 
-  angular.module("wdinstagram", [
-    "ui.router"
+  angular
+    .module("wdinstagram", [
+          "ui.router"
   ])
+    .config([
+        "$stateProvider",
+        RouterFunction
+      ])
+    .controller("WdInstaIndexController", [
+          WdInstaIndexControllerFunction
+    ])
+    .controller("WdInstaShowController", [
+      WdInstaShowControllerFunction
+    ])
+
+    function RouterFunction($stateProvider) {
+      $stateProvider
+        .state("wdinstaIndex", {
+          url: "/wdinstagrams",
+          templateUrl: "js/ng-views/index.html",
+          controller: "WdInstaIndexController",
+          controllerAs: "vm"
+        })
+        .state("wdinstaShow", {
+          url: "/wdinstagrams/:id",
+          templateUrl: "js/ng-views/show.html",
+          controller: "WdInstaShowController",
+          controllerAs: "vm"
+        })
+    }
+
+
+
+    function WdInstaIndexControllerFunction () {
+      this.wdinstagrams = wdinstagrams
+
+    }
+
+    function WdInstaShowControllerFunction ($stateParams) {
+      this.wdinstagram = wdinstagrams[$stateParams.id]
+    }
